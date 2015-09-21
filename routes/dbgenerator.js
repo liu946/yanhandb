8ce9f73af8808e1742ba10cac9ab57b0b59e15f7
liu946 <liuyang570926881@gmail.com>
2015-09-21 11:08:16 +0800

    generator fit sqllist api

--- routes/dbgenerator.js
+++ routes/dbgenerator.js
@@ -5,32 +5,34 @@
  * Copyright (c) 2015 liu. All rights reserved.
  */
 
-var express = require('express');
-var router = express.Router();
-var sqlhelper = require('../func/sql.js')
-var table = require('../model/table.js')
+ var express = require('express');
+ var router = express.Router();
+ var sqlhelper = require('../func/sql.js')
+ var table = require('../model/table.js')
 
-/* Generator the db and table */
-router.get('/', function(req, res, next) {
-	var ans = "";
+ /* Generator the db and table */
+ router.get('/', function(req, res, next) {
+ 	var ans = "";
 	// create db
 	var cdbsql = "CREATE DATABASE IF NOT EXISTS "+global.conf.dbname+" DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;";
-	ans+=cdbsql+"</br>";
-	sqlhelper.exsql(cdbsql,function(rst,field){
-		
+	
+	var cdbfoo = function () {
+		// body...
+		ans+=cdbsql+"</br>";
+	}
+	// create tables 
+	var ctbsql = "CREATE TABLE IF NOT EXISTS "+global.conf.dbname+"."+global.conf.dbtable;
+	ctbsql+="("+
+		"id int(11) not null primary key auto_increment"+
+		table.returngeneratestr()
+		+");";
 
-		// create tables 
-		var ctbsql = "CREATE TABLE IF NOT EXISTS "+global.conf.dbname+"."+global.conf.dbtable;
-		ctbsql+="("+
-			"id int(11) not null primary key auto_increment"+
-			table.returngeneratestr()
-			+");";
-		sqlhelper.exsql(ctbsql,function(rst,field){
-			ans+=ctbsql;
-			res.render('generator', { sql: ans });
-		});
-	});
+	var ctbfoo = function (){
+ 		ans+=ctbsql;
+ 		res.render('generator', { sql: ans });		
+ 	}
+	sqlhelper.exsqllist([cdbsql,ctbsql],[cdbfoo,ctbfoo]);
 });
 
-module.exports = router;
+ module.exports = router;
 

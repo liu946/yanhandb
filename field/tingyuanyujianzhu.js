ba3f4700f8638a4cd54cd36e0338d48e917d6b4e
liu946 <liuyang570926881@163.com>
2015-10-19 18:39:39 +0800

    庭院与建筑 db file

---
+++ field/tingyuanyujianzhu.js
@@ -0,0 +1,21 @@
+var sys = require('../func/sys.js')
+var childdir = './tingyuanyujianzhu/'
+var fs = require('fs')
+var tb = sys.extend(require('../base/tablefieldgroup.js'),
+    {
+    	name:"tingyuanyujianzhu",
+		namezh:"庭院与建筑",
+		fields:
+		[
+			
+
+		]
+    })
+fs.readdir(path, function (err,files) {
+	// body...
+	for (var i = files.length - 1; i >= 0; i--) {
+		this.fields.push(files[i])
+	};
+})
+require(childdir+'jianzhujibenxinxi')
+module.exports = tb;

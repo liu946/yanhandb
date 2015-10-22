5391c22508bb3060a5f4b58adb200d2a32749736
JJjie <hwj950504@163.com>
2015-10-22 16:28:04 +0800

    初步完成前端显示

--- public/script/model.coffee
+++ public/script/model.coffee
@@ -1,32 +1,31 @@
 # 将表格数据渲染模型
 
 class Model
-	constructor: (url,@target) ->
-		@inputnames = [] 								#每一个输入框的键值
-		@finalhtmlstring = ''							#最后注入的html文件字符串
-		@submitdata 									#提交后台的数据
+	constructor: (url,@target,@id) ->
+		@inputnames = []								#每一个输入框的键值
+		@finalhtmlstring 							#最后注入的html文件字符串
 		@modeldata = getdatabyajax(url).responseJSON 	#获取的表单数据
-		@dbdata 										#数据库中对应问卷的数据
 
 	# model类中的常量
 	CCS = {
 			color: {
-					'R':'rgb(255,0,0)',
-					'OR': 'rgb(255,51,0)',
-					'O': 'rgb(255,102,0)',
-					'OY': 'rgb(255,153,0)',
-					'Y': 'rgb(255,255,0)',
-					'YG': 'rgb(153,255,0)',
-					'G': 'rgb(0,255,0)',
-					'BG': 'rgb(0,255,255)',
-					'B': 'rgb(0,0,255)',
-					'BV': 'rgb(102,0,255)',
-					'V': 'rgb(255,0,255)',
-					'VR': 'rgb(255,0,102)'
+					'R':'红',
+					'OR': '橙红',
+					'O': '橙',
+					'OY': '橙黄',
+					'Y': '黄',
+					'YG': '黄绿',
+					'G': '绿'
+					'BG': '蓝绿',
+					'B': '蓝'
+					'BV': '青紫',
+					'V': '紫罗兰',
+					'VR': '紫红'
 				},
 			light: [0,1,2,3,4,5,6,7,8,9,10],
 			pure: [0,1,2,3,4,5,6,7,8,9,10]
-		} 
+		}
+	htmlstring = '' 
 	
 	# 静态方法
 	# ajax请求获取数据
@@ -54,11 +53,11 @@ class Model
 			value = ""
 
 		if type is 'input' 
-			str = "<input type='text' id='#{id}' class='input' name='#{id}' value='#{value}'/>"
+			str = "<input type='text' id='#{id}' data-type='input' class='input' name='#{id}' value='#{value}'/>"
 
 		else if type is 'select' 
 			tmp = ""
-			tmp += "<select name='#{id}' class='chosen-select select'>"
+			tmp += "<select name='#{id}' data-type='select' class='chosen-select'>"
 			selects = data.option
 			for k, v of selects
 				tmp += "<option value='#{v}' class='#{id}_#{k}'>#{v}</option>"
@@ -66,7 +65,7 @@ class Model
 
 		else if type is 'selectmult' 
 			tmp = ""
-			tmp += "<select name='#{id}' class='chosen-select selectmult' multiple>"
+			tmp += "<select name='#{id}' data-type='selectmult' class='chosen-select' multiple>"
 			selects = data.option
 			for k, v of selects
 				tmp += "<option value='#{v}' class='#{id}_#{k}'>#{v}</option>"
@@ -79,39 +78,39 @@ class Model
 			str_end = "</select></div>"
 					
 			for k,v of CCS.color
-				str_color += "<option style='background:#{v}' value='#{k}'></option>"
-				str_color += str_end
+				str_color += "<option value='#{k}'>#{v}</option>"
+			str_color += str_end
 			for i in CCS.light
 				str_light += "<option value='#{i}'>#{i}</option>"
-				str_light += str_end
+			str_light += str_end
 
 			for i in CCS.pure
 				str_pure += "<option value='#{i}'>#{i}</option>"
-				str_pure += str_end
+			str_pure += str_end
 
 			str = str_color + str_light + str_pure
 
 		else if type is 'inputornull'
 			tmp = ""
-			selects = data.option
-			for k, v of selects
-				if v is "有" || v is '其他'
-					tmp += "<input type='radio' class='inputornull other' name='#{id}' value='#{v}'/>#{v}"
-				else
-					tmp += "<input type='radio' class='inputornull' name='#{id}' value='#{v}'/>#{v}"
-			str = tmp + "<input type='text' class='otherdata' id='#{id}_other'/>"
-
-		else if type is 'selectmultornull' 
-			tmp = ""
-			tmp += "<select name='#{id}' class='chosen-select selectmultornull' multiple>"
+			tmp += "<select name='#{id}' data-type='inputornull' class='chosen-select'>"
 			selects = data.option
 			for k, v of selects
 				if v is "有" || v is '其他'
 					tmp += "<option value='#{v}' class='#{id}_#{k} other'>#{v}</option>"
 				else
 					tmp += "<option value='#{v}' class='#{id}_#{k}'>#{v}</option>"
-				
-			str = tmp + "</select>" + "<input type='text' class='otherdata' id='#{id}_other'/>"
+			str = tmp + "<input type='text' class='otherdata' id='#{id}_other'/>"
+
+		else if type is 'selectmultornull' 
+			tmp = "<input id='#{id}' name='#{id}' type='checkbox'>有<select name='#{id}_other' class='chosen-select otherdata' multiple>"
+			selects = data.option
+			for k, v of selects
+				tmp += "<option value='#{v}' class='#{id}_#{k}'>#{v}</option>"
+			str = tmp + "</select>"
+
+		else if type is 'boolean'
+			str = "<input type='radio' class='inputornull' name='#{id}' value='true'/>是
+				<input type='radio' class='inputornull' name='#{id}' value='false'/>否"
 
 		else
 			str = ''
@@ -120,67 +119,184 @@ class Model
 
 	# 遍历表单类型渲染数据
 	# 遍历n叉树
-	modelstack = new Array() # 状态栈
-	flagstack = new Array() # 标记栈
-	flag = 0 # 0号位
-	ergodicdata = (modeldata,inputnames,htmlstring) ->
-		len = modeldata.length
-		i = 0
-		for a in modeldata
-			console.log a
-			if a.forend is undefined && a.fields isnt undefined
-				title = a.namezh
-				htmlstring += "<div id='#{a.name}' class='J_content'>
-							<h3>#{title}</h3></div>"
+	ergodicdata = (modeldata,position,inputnames,flag) ->
+		target = modeldata[position]
+		if target.forend isnt undefined && target.fields is undefined
+			title = target.namezh
+			str = gethtmlstring target.forend,target.name
+			inputnames = buildkeys target.name,inputnames
 
-				# 压入栈
-				modelstack.push modeldata
-				flagstack.push flag
-				flag += 1 
-				modeldata = a.fields
+			htmlstring += "<div class='list'>
+							<div class='note'>
+								<h4>#{title}</h4>
+							</div>
+							<div class='shuru'>
+								#{str}
+							</div>
+						</div>"
+			return
+		else
+			len = target.fields.length
+			for i in [0...len] by 1
+				if flag is 0
+					title = target.namezh
+					htmlstring += "<div id='#{target.name}' class='J_content'>
+								<h3>#{title}</h3></div><div class='clear'></div><hr />"
+					flag = 1
 
-				ergodicdata modeldata,inputnames,htmlstring
+				ergodicdata target.fields,i,inputnames,flag
 				
+	# 判断选择框中需要手动输入类型的情况
+	judgeother = (point) ->
+		id = point.id
+		type = point.name
+		check = point.type
+		input = $("##{type}_other")
 
-			else if a.forend isnt undefined && a.fields is undefined
-				title = a.namezh
-				str = gethtmlstring a.forend,a.name
-				inputnames = buildkeys a.name,inputnames
-
-				htmlstring += "<div class='list'>
-								<div class='note'>
-									<h4>#{title}</h4>
-								</div>
-								<div class='shuru'>
-									#{str}
-								</div>
-							</div>"
-				# 删叶子节点
-				delete modeldata[i]
-				i += 1
-				if i == len
-					modeldata = modelstack.pop()
-					# 删根节点
-					flag = flagstack.pop()
-					delete modeldata[flag]
-
-					ergodicdata modeldata,inputnames,htmlstring
-				
+		$(point).on "change",() ->
+			flag = 0
+			if check is 'checkbox'
+				if $(point).prop('checked')
+					flag = 1
+				else
+					flag = 0
 			else
-				htmlstring += ""
-				i += 1
-				if i == len
-					modeldata = modelstack.pop()
-				return {
-					modeldata: modeldata,
-					inputnames: inputnames,
-					htmlstring: htmlstring
-				}
+				value = $(point).val()
+				if value != null
+					if typeof value is 'string'
+						if value == "其他" || value == "有"
+							flag = 1
+					else if typeof value is 'object'
+						for t in $(point).val()
+							if t == "其他" || t == "有"
+								flag = 1
+					else
+						flag = 0
+
+			if flag
+				input.css 'display','inline-block'
+			else
+				input.css 'display','none'
+				
+	# 判断值类型与存储方式的工具
+	# 检查传入值的正确性
+	builddbvalue = (type,value='',idp='') ->
+		flag = 0 # 判断是否满足提交条件
+		if type is ('input' or 'boolean' or 'select')
+			if value isnt ("" or undefined)
+				dbvalue = "#{type}_#{value}"
+				flag = 1
+
+		else if type is 'selectmult'
+			if value instanceof 'Array'
+				dbvalue = "#{type}_"
+				for i in value
+					dbvalue += "#{i}&"
+				flag = 1
+
+		else if type is 'CCS'
+			dbvalue = "CCS_"
+			color = $("select[name=#{i}_color]").val() 
+			light = $("select[name=#{i}_light]").val() 
+			pure =  $("select[name=#{i}_pure]").val()
+			dbvalue += color + "&" + light + "&" + pure
+			flag = 1
+
+		else if type is 'selectmultornull'
+			target = $("input##{idp}[type=checkbox]")
+			dbvalue = "#{type}_"
+			if target.prop('checked')
+				value = $("select[name=##{idp}_other]").val()
+				if value instanceof 'Array'
+					for i in value
+						dbvalue += "#{i}&"
+				else
+					dbvalue = "#{type}_null"
+			else
+				dbvalue = "#{type}_null"
+			flag = 1
+
+		else if type is 'inputornull'
+			dbvalue = "#{type}_"
+			if value is ('其他' or '有')
+				othervalue = $("##{idp}_other").val()
+				if othervalue is ("" or null)
+					dbvalue += "#{value}-"
+				else
+					dbvalue += "#{value}-othervalue"
+				flag = 1
+			else if value isnt ("" or undefined)
+				dbvalue += "#{value}"
+				flag = 1
+
+		return {
+			dbvalue: dbvalue,
+			flag: flag
+		}
+
+	# 获取表单中目前的所有值情况
+	getformvalue = (form,inputnames,editid) ->
+		data = {"id": editid}
+		flag = [] # 标注哪一位出现输入不完整
+		for i in inputnames
+			target = $("##{form} input##{i}[type=text]") #input
+			if target.length < 1
+				target = $("##{form} input[name=#{i}]:checked") #boolean
+				if target.length < 1
+					target = $("##{form} input##{i}[type=checkbox]") #selectmultornull
+					if target.length < 1
+						target = $("##{form} select[name=#{i}]") #select,selectmult,inputornull
+						if target.length < 1 #CCS
+							result = builddbvalue 'CCS',"",i
+						else
+							datatype = target.data("data")
+							result = builddbvalue datatype,target.val(),i
+					else
+						result = builddbvalue 'selectmultornull',"",i
+				else
+					result = builddbvalue 'boolean',target.val()
+			else
+				result = builddbvalue 'input',target.val()
+
+			
+			data[i] = result.dbvalue
+			flag.push result.flag
+
+		jundge = true
+		for f in flag
+			if f is 0
+				jundge = false
+				break;
+		
+		return {
+			data : data
+			jundge : jundge
+		}
 
 	# 初始化
 	init: () ->
-		ergodicdata @modeldata,@inputnames,@finalhtmlstring
+		len = @modeldata.length
+		for i in [0...len] by 1
+			ergodicdata @modeldata,i,@inputnames,0
+
+		@finalhtmlstring = htmlstring
 		console.log @modeldata,@inputnames
-		@target.append @finalhtmlstring
+		$("##{@target}").append @finalhtmlstring
+
+	# 获取数据库数据
+	getdbdata: () ->
+
+
+
+	savedata: (clickbtn) ->
+		$("##{clickbtn}").on 'click', () ->
+			result = getformvalue @target,@inputnames
+
+			$.post '/input/update', result.data, (data) ->
+				alert '保存成功'
+			
+			return result.jundge
+
+
 
 
--- public/script/model.js
+++ public/script/model.js
@@ -2,35 +2,36 @@
 var Model;
 
 Model = (function() {
-  var CCS, buildkeys, ergodicdata, flag, flagstack, getdatabyajax, gethtmlstring, modelstack;
+  var CCS, builddbvalue, buildkeys, ergodicdata, getdatabyajax, getformvalue, gethtmlstring, htmlstring, judgeother;
 
-  function Model(url, target1) {
+  function Model(url, target1, id1) {
     this.target = target1;
+    this.id = id1;
     this.inputnames = [];
-    this.finalhtmlstring = '';
-    this.submitdata;
+    this.finalhtmlstring;
     this.modeldata = getdatabyajax(url).responseJSON;
-    this.dbdata;
   }
 
   CCS = {
     color: {
-      'R': 'rgb(255,0,0)',
-      'OR': 'rgb(255,51,0)',
-      'O': 'rgb(255,102,0)',
-      'OY': 'rgb(255,153,0)',
-      'Y': 'rgb(255,255,0)',
-      'YG': 'rgb(153,255,0)',
-      'G': 'rgb(0,255,0)',
-      'BG': 'rgb(0,255,255)',
-      'B': 'rgb(0,0,255)',
-      'BV': 'rgb(102,0,255)',
-      'V': 'rgb(255,0,255)',
-      'VR': 'rgb(255,0,102)'
+      'R': '红',
+      'OR': '橙红',
+      'O': '橙',
+      'OY': '橙黄',
+      'Y': '黄',
+      'YG': '黄绿',
+      'G': '绿',
+      'BG': '蓝绿',
+      'B': '蓝',
+      'BV': '青紫',
+      'V': '紫罗兰',
+      'VR': '紫红'
     },
     light: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
     pure: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
   };
+
+  htmlstring = '';
 
   getdatabyajax = function(url) {
     return $.ajax({
@@ -64,10 +65,10 @@ Model = (function() {
       value = "";
     }
     if (type === 'input') {
-      str = "<input type='text' id='" + id + "' class='input' name='" + id + "' value='" + value + "'/>";
+      str = "<input type='text' id='" + id + "' data-type='input' class='input' name='" + id + "' value='" + value + "'/>";
     } else if (type === 'select') {
       tmp = "";
-      tmp += "<select name='" + id + "' class='chosen-select select'>";
+      tmp += "<select name='" + id + "' data-type='select' class='chosen-select'>";
       selects = data.option;
       for (k in selects) {
         v = selects[k];
@@ -76,7 +77,7 @@ Model = (function() {
       str = tmp + "</select>";
     } else if (type === 'selectmult') {
       tmp = "";
-      tmp += "<select name='" + id + "' class='chosen-select selectmult' multiple>";
+      tmp += "<select name='" + id + "' data-type='selectmult' class='chosen-select' multiple>";
       selects = data.option;
       for (k in selects) {
         v = selects[k];
@@ -91,37 +92,25 @@ Model = (function() {
       ref = CCS.color;
       for (k in ref) {
         v = ref[k];
-        str_color += "<option style='background:" + v + "' value='" + k + "'></option>";
-        str_color += str_end;
+        str_color += "<option value='" + k + "'>" + v + "</option>";
       }
+      str_color += str_end;
       ref1 = CCS.light;
       for (j = 0, len1 = ref1.length; j < len1; j++) {
         i = ref1[j];
         str_light += "<option value='" + i + "'>" + i + "</option>";
-        str_light += str_end;
       }
+      str_light += str_end;
       ref2 = CCS.pure;
       for (l = 0, len2 = ref2.length; l < len2; l++) {
         i = ref2[l];
         str_pure += "<option value='" + i + "'>" + i + "</option>";
-        str_pure += str_end;
       }
+      str_pure += str_end;
       str = str_color + str_light + str_pure;
     } else if (type === 'inputornull') {
       tmp = "";
-      selects = data.option;
-      for (k in selects) {
-        v = selects[k];
-        if (v === "有" || v === '其他') {
-          tmp += "<input type='radio' class='inputornull other' name='" + id + "' value='" + v + "'/>" + v;
-        } else {
-          tmp += "<input type='radio' class='inputornull' name='" + id + "' value='" + v + "'/>" + v;
-        }
-      }
-      str = tmp + ("<input type='text' class='otherdata' id='" + id + "_other'/>");
-    } else if (type === 'selectmultornull') {
-      tmp = "";
-      tmp += "<select name='" + id + "' class='chosen-select selectmultornull' multiple>";
+      tmp += "<select name='" + id + "' data-type='inputornull' class='chosen-select'>";
       selects = data.option;
       for (k in selects) {
         v = selects[k];
@@ -131,66 +120,226 @@ Model = (function() {
           tmp += "<option value='" + v + "' class='" + id + "_" + k + "'>" + v + "</option>";
         }
       }
-      str = tmp + "</select>" + ("<input type='text' class='otherdata' id='" + id + "_other'/>");
+      str = tmp + ("<input type='text' class='otherdata' id='" + id + "_other'/>");
+    } else if (type === 'selectmultornull') {
+      tmp = "<input id='" + id + "' name='" + id + "' type='checkbox'>有<select name='" + id + "_other' class='chosen-select otherdata' multiple>";
+      selects = data.option;
+      for (k in selects) {
+        v = selects[k];
+        tmp += "<option value='" + v + "' class='" + id + "_" + k + "'>" + v + "</option>";
+      }
+      str = tmp + "</select>";
+    } else if (type === 'boolean') {
+      str = "<input type='radio' class='inputornull' name='" + id + "' value='true'/>是 <input type='radio' class='inputornull' name='" + id + "' value='false'/>否";
     } else {
       str = '';
     }
     return str;
   };
 
-  modelstack = new Array();
-
-  flagstack = new Array();
-
-  flag = 0;
-
-  ergodicdata = function(modeldata, inputnames, htmlstring) {
-    var a, i, j, len, len1, str, title;
-    len = modeldata.length;
-    i = 0;
-    for (j = 0, len1 = modeldata.length; j < len1; j++) {
-      a = modeldata[j];
-      console.log(a);
-      if (a.forend === void 0 && a.fields !== void 0) {
-        title = a.namezh;
-        htmlstring += "<div id='" + a.name + "' class='J_content'> <h3>" + title + "</h3></div>";
-        modelstack.push(modeldata);
-        flagstack.push(flag);
-        flag += 1;
-        modeldata = a.fields;
-        ergodicdata(modeldata, inputnames, htmlstring);
-      } else if (a.forend !== void 0 && a.fields === void 0) {
-        title = a.namezh;
-        str = gethtmlstring(a.forend, a.name);
-        inputnames = buildkeys(a.name, inputnames);
-        htmlstring += "<div class='list'> <div class='note'> <h4>" + title + "</h4> </div> <div class='shuru'> " + str + " </div> </div>";
-        delete modeldata[i];
-        i += 1;
-        if (i === len) {
-          modeldata = modelstack.pop();
-          flag = flagstack.pop();
-          delete modeldata[flag];
-          ergodicdata(modeldata, inputnames, htmlstring);
+  ergodicdata = function(modeldata, position, inputnames, flag) {
+    var i, j, len, ref, results, str, target, title;
+    target = modeldata[position];
+    if (target.forend !== void 0 && target.fields === void 0) {
+      title = target.namezh;
+      str = gethtmlstring(target.forend, target.name);
+      inputnames = buildkeys(target.name, inputnames);
+      htmlstring += "<div class='list'> <div class='note'> <h4>" + title + "</h4> </div> <div class='shuru'> " + str + " </div> </div>";
+    } else {
+      len = target.fields.length;
+      results = [];
+      for (i = j = 0, ref = len; j < ref; i = j += 1) {
+        if (flag === 0) {
+          title = target.namezh;
+          htmlstring += "<div id='" + target.name + "' class='J_content'> <h3>" + title + "</h3></div><div class='clear'></div><hr />";
+          flag = 1;
         }
-      } else {
-        htmlstring += "";
-        i += 1;
-        if (i === len) {
-          modeldata = modelstack.pop();
-        }
-        return {
-          modeldata: modeldata,
-          inputnames: inputnames,
-          htmlstring: htmlstring
-        };
+        results.push(ergodicdata(target.fields, i, inputnames, flag));
       }
+      return results;
     }
   };
 
+  judgeother = function(point) {
+    var check, id, input, type;
+    id = point.id;
+    type = point.name;
+    check = point.type;
+    input = $("#" + type + "_other");
+    return $(point).on("change", function() {
+      var flag, j, len1, ref, t, value;
+      flag = 0;
+      if (check === 'checkbox') {
+        if ($(point).prop('checked')) {
+          flag = 1;
+        } else {
+          flag = 0;
+        }
+      } else {
+        value = $(point).val();
+        if (value !== null) {
+          if (typeof value === 'string') {
+            if (value === "其他" || value === "有") {
+              flag = 1;
+            }
+          } else if (typeof value === 'object') {
+            ref = $(point).val();
+            for (j = 0, len1 = ref.length; j < len1; j++) {
+              t = ref[j];
+              if (t === "其他" || t === "有") {
+                flag = 1;
+              }
+            }
+          } else {
+            flag = 0;
+          }
+        }
+      }
+      if (flag) {
+        return input.css('display', 'inline-block');
+      } else {
+        return input.css('display', 'none');
+      }
+    });
+  };
+
+  builddbvalue = function(type, value, idp) {
+    var color, dbvalue, flag, i, j, l, len1, len2, light, othervalue, pure, target;
+    if (value == null) {
+      value = '';
+    }
+    if (idp == null) {
+      idp = '';
+    }
+    flag = 0;
+    if (type === ('input' || 'boolean' || 'select')) {
+      if (value !== ("" || void 0)) {
+        dbvalue = type + "_" + value;
+        flag = 1;
+      }
+    } else if (type === 'selectmult') {
+      if (value instanceof 'Array') {
+        dbvalue = type + "_";
+        for (j = 0, len1 = value.length; j < len1; j++) {
+          i = value[j];
+          dbvalue += i + "&";
+        }
+        flag = 1;
+      }
+    } else if (type === 'CCS') {
+      dbvalue = "CCS_";
+      color = $("select[name=" + i + "_color]").val();
+      light = $("select[name=" + i + "_light]").val();
+      pure = $("select[name=" + i + "_pure]").val();
+      dbvalue += color + "&" + light + "&" + pure;
+      flag = 1;
+    } else if (type === 'selectmultornull') {
+      target = $("input#" + idp + "[type=checkbox]");
+      dbvalue = type + "_";
+      if (target.prop('checked')) {
+        value = $("select[name=#" + idp + "_other]").val();
+        if (value instanceof 'Array') {
+          for (l = 0, len2 = value.length; l < len2; l++) {
+            i = value[l];
+            dbvalue += i + "&";
+          }
+        } else {
+          dbvalue = type + "_null";
+        }
+      } else {
+        dbvalue = type + "_null";
+      }
+      flag = 1;
+    } else if (type === 'inputornull') {
+      dbvalue = type + "_";
+      if (value === ('其他' || '有')) {
+        othervalue = $("#" + idp + "_other").val();
+        if (othervalue === ("" || null)) {
+          dbvalue += value + "-";
+        } else {
+          dbvalue += value + "-othervalue";
+        }
+        flag = 1;
+      } else if (value !== ("" || void 0)) {
+        dbvalue += "" + value;
+        flag = 1;
+      }
+    }
+    return {
+      dbvalue: dbvalue,
+      flag: flag
+    };
+  };
+
+  getformvalue = function(form, inputnames, editid) {
+    var data, datatype, f, flag, i, j, jundge, l, len1, len2, result, target;
+    data = {
+      "id": editid
+    };
+    flag = [];
+    for (j = 0, len1 = inputnames.length; j < len1; j++) {
+      i = inputnames[j];
+      target = $("#" + form + " input#" + i + "[type=text]");
+      if (target.length < 1) {
+        target = $("#" + form + " input[name=" + i + "]:checked");
+        if (target.length < 1) {
+          target = $("#" + form + " input#" + i + "[type=checkbox]");
+          if (target.length < 1) {
+            target = $("#" + form + " select[name=" + i + "]");
+            if (target.length < 1) {
+              result = builddbvalue('CCS', "", i);
+            } else {
+              datatype = target.data("data");
+              result = builddbvalue(datatype, target.val(), i);
+            }
+          } else {
+            result = builddbvalue('selectmultornull', "", i);
+          }
+        } else {
+          result = builddbvalue('boolean', target.val());
+        }
+      } else {
+        result = builddbvalue('input', target.val());
+      }
+      data[i] = result.dbvalue;
+      flag.push(result.flag);
+    }
+    jundge = true;
+    for (l = 0, len2 = flag.length; l < len2; l++) {
+      f = flag[l];
+      if (f === 0) {
+        jundge = false;
+        break;
+      }
+    }
+    return {
+      data: data,
+      jundge: jundge
+    };
+  };
+
   Model.prototype.init = function() {
-    ergodicdata(this.modeldata, this.inputnames, this.finalhtmlstring);
+    var i, j, len, ref;
+    len = this.modeldata.length;
+    for (i = j = 0, ref = len; j < ref; i = j += 1) {
+      ergodicdata(this.modeldata, i, this.inputnames, 0);
+    }
+    this.finalhtmlstring = htmlstring;
     console.log(this.modeldata, this.inputnames);
-    return this.target.append(this.finalhtmlstring);
+    return $("#" + this.target).append(this.finalhtmlstring);
+  };
+
+  Model.prototype.getdbdata = function() {};
+
+  Model.prototype.savedata = function(clickbtn) {
+    return $("#" + clickbtn).on('click', function() {
+      var result;
+      result = getformvalue(this.target, this.inputnames);
+      $.post('/input/update', result.data, function(data) {
+        return alert('保存成功');
+      });
+      return result.jundge;
+    });
   };
 
   return Model;

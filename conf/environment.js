4390a1e7412bfe768491faae07c8e81902dd1ec3
liu946 <liuyang570926881@163.com>
2015-10-24 19:23:10 +0800

    static file

--- conf/environment.js
+++ conf/environment.js
@@ -9,7 +9,7 @@ module.exports = function (app) {
 
     app.set('views', path.join(__dirname, '../views'));
 	app.set('view engine', 'jade');
-
+    app.use(express.static( path.join(__dirname,"../public")))
 	app.use(logger('dev'));
 	app.use(bodyParser.json());
 	app.use(bodyParser.urlencoded({ extended: false }));

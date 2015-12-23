fdcc00a29bfcde9d371d53bab16eae03edfba070
liu946 <mkliuyag@163.com>
2015-12-23 11:54:44 +0800

    alter script

---
+++ yanhanAlter.sql
@@ -0,0 +1,9 @@
+
+UPDATE `tingyuanyujianzhu` SET `JZSNHJRHJWQBWGRLeiXing`='',`JZSNHJRHJWQBWGRCaiLiao`='',`JZSNHJRHJWQBWGRHouDu`='' WHERE `JZSNHJRHJWWHJGGRYBWYouWuWaiQiangBaoWenGeRe`=0;
+UPDATE `tingyuanyujianzhu` SET `JZSNHJRHJWDBWGRLeiXing`='',`JZSNHJRHJWDBWGRCaiLiao`='',`JZSNHJRHJWDBWGRHouDu`='' WHERE `JZSNHJRHJYouWuWuDingBaoWenGeRe`=0;
+UPDATE `tingyuanyujianzhu` SET `JZWBTZTJBiaoMianCaiLiao`='',`JZWBTZTJYanSe`='' WHERE `JZWBTZYouWuTaiJi`=0;
+
+UPDATE `tingyuanyujianzhu` SET `JZJBXXZongJianZhuMianJi`=NULL WHERE `JZJBXXZongJianZhuMianJi`=0;
+UPDATE `tingyuanyujianzhu` SET `JZJBXXJianZhuJiDiMianJi`=NULL WHERE `JZJBXXJianZhuJiDiMianJi`=0;
+UPDATE `tingyuanyujianzhu` SET `JZJBXXDiXiaKongJianMianJi`=NULL WHERE `JZJBXXDiXiaKongJianMianJi`=0;
+UPDATE `tingyuanyujianzhu` SET `JZJBXXTingYuanMianJi`=NULL WHERE `JZJBXXTingYuanMianJi`=0;
